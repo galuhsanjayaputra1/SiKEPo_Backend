9a77767e8f13f5e6be2b2aef2b4905ef430e8655
Galuh Sanjaya Putra <galuhputra1308@gmail.com>
2026-09-08 09:55:05 +0700

    fix: prevent auto migration on existing users table

--- config/database.go
+++ config/database.go
@@ -15,7 +15,7 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	// Load .env file
+	// Load .env
 	err := godotenv.Load()
 	if err != nil {
 		log.Println("Error loading .env file, using default environment variables")
@@ -27,17 +27,37 @@ func ConnectDatabase() {
 	dbPassword := os.Getenv("DB_PASSWORD")
 	dbName := os.Getenv("DB_NAME")
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPassword, dbHost, dbPort, dbName)
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		dbUser,
+		dbPassword,
+		dbHost,
+		dbPort,
+		dbName,
+	)
 
 	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-
 	if err != nil {
 		panic(fmt.Sprintf("Failed to connect to database: %v", err))
 	}
 
-	// Auto Migrate the models
-	database.AutoMigrate(&models.User{})
+	// Cek apakah tabel users sudah ada
+	hasTable := database.Migrator().HasTable(&models.User{})
+
+	if !hasTable {
+		log.Println("Table 'users' belum ada. Membuat table...")
+
+		err := database.AutoMigrate(&models.User{})
+		if err != nil {
+			panic(fmt.Sprintf("Failed to migrate users table: %v", err))
+		}
+
+		log.Println("Table 'users' berhasil dibuat!")
+	} else {
+		log.Println("Table 'users' sudah ada. AutoMigrate dilewati.")
+	}
 
 	DB = database
+
 	log.Println("Database connected successfully!")
 }
